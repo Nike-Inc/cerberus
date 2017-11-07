a92439db9234102bd49f4d5c26108f979272b3c7
Todd Lisonbee <tlisonbee@users.noreply.github.com>
2017-11-07 13:55:09 -0800

    Additional configuration options for auth token generation and hashing, plus related cleanup (#85)

--- src/main/java/com/nike/cerberus/util/AuthTokenGenerator.java
+++ src/main/java/com/nike/cerberus/util/AuthTokenGenerator.java
@@ -17,19 +17,23 @@
 
 package com.nike.cerberus.util;
 
+import javax.inject.Inject;
+import javax.inject.Named;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Locale;
 
 /**
- * Generate an Auth Token
+ * Generate an Auth Token.
+ * <p>
+ * Auth tokens have a configurable TTL, e.g. 1 hour.
  * <p>
  * References:
  * https://stackoverflow.com/questions/41107/how-to-generate-a-random-alpha-numeric-string/41156#41156
  */
 public class AuthTokenGenerator {
 
-    // TODO: additional research, security review
+    public static final String AUTH_TOKEN_LENGTH_CONFIG_PARAM = "cms.auth.token.generate.length";
 
     private static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static final String LOWER = UPPER.toLowerCase(Locale.ROOT);
@@ -38,18 +42,23 @@ public class AuthTokenGenerator {
 
     private static final char[] SYMBOLS = ALPHANUM.toCharArray();
 
-    private static final int LENGTH = 64;
+    private final int length;
 
     private final SecureRandom random = SecureRandom.getInstanceStrong();
 
-    public AuthTokenGenerator() throws NoSuchAlgorithmException {
+    @Inject
+    public AuthTokenGenerator(@Named(AUTH_TOKEN_LENGTH_CONFIG_PARAM) final int length) throws NoSuchAlgorithmException {
+        if (length < 64) {
+            throw new IllegalArgumentException(AUTH_TOKEN_LENGTH_CONFIG_PARAM + " must be at least 64 but was " + length);
+        }
+        this.length = length;
     }
 
     /**
      * Generate an Auth Token
      */
     public String generateSecureToken() {
-        final char[] buf = new char[LENGTH];
+        final char[] buf = new char[length];
         for (int i = 0; i < buf.length; ++i) {
             buf[i] = SYMBOLS[random.nextInt(SYMBOLS.length)];
         }
