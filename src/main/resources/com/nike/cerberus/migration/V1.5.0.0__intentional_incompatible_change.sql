6eca4aae7794975356d87434e304a5c0f6987e3a
Todd Lisonbee <todd.lisonbee@nike.com>
2017-11-08 15:37:25 -0800

    Adding intentional incompatible migration script to protect against accidentally deploying the wrong CMS AMI in the wrong environment.

---
+++ src/main/resources/com/nike/cerberus/migration/V1.5.0.0__intentional_incompatible_change.sql
@@ -0,0 +1,15 @@
+#
+# This is a dummy migration script.  It is was created to introduce an intentional incompatible
+# change in the database between the new and old Cerberus architecture.  This will cause CMS deployment
+# to fail if someone simply tries to deploy the next generation of a CMS AMI over the previous
+# generation instead of using the correct migration path (e.g. standing up a whole new environment,
+# copying the data over, updating DNS).
+#
+# For local development, you can get past the introduced issue by dropping all of your data with the
+# gradle command `./gradlew flywayClean`.
+#
+# More here:
+# https://flywaydb.org/documentation/gradle/
+#
+# Next generation of Cerberus architecture being released in 2018.
+#
