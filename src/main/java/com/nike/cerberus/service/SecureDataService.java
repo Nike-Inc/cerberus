6a79432dcd5e42320680c335b8d03a79a8ce6eb8
Shaun Ford <shaun.ford@nike.com>
2018-02-16 14:49:18 -0800

    Fix bug in boolean logic for current secret versions

--- src/main/java/com/nike/cerberus/service/SecureDataService.java
+++ src/main/java/com/nike/cerberus/service/SecureDataService.java
@@ -248,9 +248,11 @@ public class SecureDataService {
     /**
      * @return  True if the secret has ever been updated, false if it has not.
      */
-    public boolean hasSecureDataBeenUpdated(SecureDataRecord secureDataRecord) {
-        return secureDataRecord.getCreatedBy().equals(secureDataRecord.getLastUpdatedBy()) &&
-                secureDataRecord.getCreatedTs() == secureDataRecord.getLastUpdatedTs();
+    boolean secureDataHasBeenUpdated(SecureDataRecord secureDataRecord) {
+        boolean createdBySameAsUpdatedBy = secureDataRecord.getCreatedBy().equals(secureDataRecord.getLastUpdatedBy());
+        boolean createdTsSameAsUpdatedTs = secureDataRecord.getCreatedTs() == secureDataRecord.getLastUpdatedTs();
+
+        return ! (createdBySameAsUpdatedBy && createdTsSameAsUpdatedTs);
     }
 
     public Optional<SecureDataRecord> getSecureDataRecordForPath(String path) {
