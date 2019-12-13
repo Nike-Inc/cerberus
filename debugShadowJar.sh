dc5df656a0a0b2a8c9c490fbb8dff5d33ba95abd
Justin Field <fieldju@gmail.com>
2019-12-13 08:30:24 -0800

    chore: Burn everything to the ground so a phoenix may arise from the ashes. Deleted all the files so that we can bootstrap a new project but retain our history.

--- debugShadowJar.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -jar -D@appId=cms -D@environment=local $* -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005 build/libs/*.jar
--- src/main/resources/com/nike/cerberus/migration/V1.6.1.0__truncate_secure_data_version_table.sql
+++
@@ -1,2 +0,0 @@
--- Clear out the secure data table to delete bad version data.
-TRUNCATE TABLE SECURE_DATA_VERSION;
