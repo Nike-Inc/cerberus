fd8c00839b0809b9388193c70e9f43d5c3c5fd5e
Justin Field <fieldju@gmail.com>
2020-01-23 14:22:37 -0800

    fix: support double slashes when recieving requests

--- cerberus-web/src/main/java/com/nike/cerberus/security/WebSecurityConfiguration.java
+++ cerberus-web/src/main/java/com/nike/cerberus/security/WebSecurityConfiguration.java
@@ -11,10 +11,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.firewall.HttpFirewall;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.OrRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
@@ -48,6 +50,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Autowired private SpringApiExceptionHandler springApiExceptionHandler;
 
+  @Autowired HttpFirewall allowUrlEncodedSlashHttpFirewall;
+
   RequestMatcher getDoesRequestsRequireAuthMatcher() {
 
     List<RequestMatcher> whiteListMatchers =
@@ -56,6 +60,14 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
             .collect(Collectors.toList());
     var whiteListMatcher = new OrRequestMatcher(whiteListMatchers);
     return request -> !whiteListMatcher.matches(request);
+  }
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    super.configure(web);
+
+    // Allow double / in URIs to support buggy Cerberus clients that worked with Highland arch.
+    web.httpFirewall(allowUrlEncodedSlashHttpFirewall);
   }
 
   @Override
