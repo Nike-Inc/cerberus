b53c20b39823e351fa798798c08da18cef709bf6
Rebecca Dong <rebecca.dong@nike.com>
2021-03-03 11:20:05 -0800

    porting more JWT

---
+++ cerberus-web/src/main/java/com/nike/cerberus/mapper/JwtBlacklistMapper.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2019 Nike, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nike.cerberus.mapper;
+
+import com.nike.cerberus.record.JwtBlacklistRecord;
+import java.util.HashSet;
+import org.apache.ibatis.annotations.Param;
+
+public interface JwtBlacklistMapper {
+
+  HashSet<String> getBlacklist();
+
+  int addToBlacklist(@Param("record") JwtBlacklistRecord record);
+
+  int deleteExpiredTokens();
+}
