b6b0a9c83b49ad9604e83a81ae817fa884a8d980
Rebecca Dong <rebecca.dong@nike.com>
2021-03-05 15:02:43 -0800

    jwt port wip, refresh token failing

---
+++ cerberus-web/src/main/java/com/nike/cerberus/jobs/JwtBlacklistRefreshJob.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2019 Nike, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.nike.cerberus.jobs;
+
+/** Periodically refresh JWT blacklist. */
+public class JwtBlacklistRefreshJob {
+  // TODO: Port this class
+  //    private static final Logger log = LoggerFactory.getLogger(JwtBlacklistRefreshJob.class);
+  //
+  //    private final JwtService jwtService;
+  //
+  //    @Inject
+  //    public JwtBlacklistRefreshJob(JwtService jwtService) {
+  //        this.jwtService = jwtService;
+  //    }
+  //
+  //    public void doRun()  {
+  //        log.debug("Running JWT blacklist refresh job");
+  //        try {
+  //            jwtService.refreshBlacklist();
+  //        } catch (Exception e) {
+  //            log.warn("Error refreshing JWT blacklist", e);
+  //        }
+  //    }
+}
