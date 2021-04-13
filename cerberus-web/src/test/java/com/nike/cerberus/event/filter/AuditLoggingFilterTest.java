109b8eba84a39b65158bf22072913e5a492f15cf
RVelp1 <ranjithreddy.velpur@nike.com>
2021-04-13 12:43:41 -0700

    Addressed PR comments made by Shawn Sherwood

--- cerberus-web/src/test/java/com/nike/cerberus/event/filter/AuditLoggingFilterTest.java
+++ cerberus-web/src/test/java/com/nike/cerberus/event/filter/AuditLoggingFilterTest.java
@@ -1,13 +1,11 @@
 package com.nike.cerberus.event.filter;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 import com.nike.cerberus.util.SdbAccessRequest;
-import java.io.IOException;
 import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.junit.After;
@@ -61,14 +59,19 @@ public class AuditLoggingFilterTest {
   }
 
   @Test
-  public void testDoFilterInternal() throws IOException, ServletException {
-    doNothing().when(filterChain).doFilter(request, response);
-    when(request.getMethod()).thenReturn("GET");
-    when(request.getServletPath()).thenReturn("path");
-    mockxFwdAndVersion();
-    when(auditLoggingFilterDetails.getSdbNameSlug()).thenReturn("sdbNameSlug");
-    doNothing().when(applicationEventPublisher).publishEvent(ApplicationEvent.class);
-    auditLoggingFilter.doFilterInternal(request, response, filterChain);
+  public void testDoFilterInternal() {
+    try {
+      doNothing().when(filterChain).doFilter(request, response);
+      when(request.getMethod()).thenReturn("GET");
+      when(request.getServletPath()).thenReturn("path");
+      mockxFwdAndVersion();
+      when(auditLoggingFilterDetails.getSdbNameSlug()).thenReturn("sdbNameSlug");
+      doNothing().when(applicationEventPublisher).publishEvent(ApplicationEvent.class);
+      auditLoggingFilter.doFilterInternal(request, response, filterChain);
+      assertNotNull(SecurityContextHolder.getContext());
+    } catch (Exception exception) {
+      assertNull(exception);
+    }
   }
 
   private void mockxFwdAndVersion() {
@@ -79,27 +82,35 @@ public class AuditLoggingFilterTest {
   }
 
   @Test
-  public void testDoFilterInternal_pass_sdbslug_sdbAccessRequest()
-      throws IOException, ServletException {
-    doNothing().when(filterChain).doFilter(request, response);
-    Mockito.when(request.getMethod()).thenReturn("GET");
-    Mockito.when(request.getServletPath()).thenReturn("path");
-    mockxFwdAndVersion();
-    Mockito.when(sdbAccessRequest.getSdbSlug()).thenReturn("sdbNameSlug");
-    doNothing().when(applicationEventPublisher).publishEvent(ApplicationEvent.class);
-    auditLoggingFilter.doFilterInternal(request, response, filterChain);
+  public void testDoFilterInternal_pass_sdbslug_sdbAccessRequest() {
+    try {
+      doNothing().when(filterChain).doFilter(request, response);
+      Mockito.when(request.getMethod()).thenReturn("GET");
+      Mockito.when(request.getServletPath()).thenReturn("path");
+      mockxFwdAndVersion();
+      Mockito.when(sdbAccessRequest.getSdbSlug()).thenReturn("sdbNameSlug");
+      doNothing().when(applicationEventPublisher).publishEvent(ApplicationEvent.class);
+      auditLoggingFilter.doFilterInternal(request, response, filterChain);
+      assertNotNull(SecurityContextHolder.getContext());
+    } catch (Exception exception) {
+      assertNull(exception);
+    }
   }
 
   @Test
-  public void testDoFilterInternal_actionpassed_auditloggingfilterdetails()
-      throws IOException, ServletException {
-    doNothing().when(filterChain).doFilter(request, response);
-    Mockito.when(auditLoggingFilterDetails.getAction())
-        .thenReturn("principal" + " " + "GET" + "path");
-    mockxFwdAndVersion();
-    Mockito.when(auditLoggingFilterDetails.getSdbNameSlug()).thenReturn("sdbNameSlug");
-    doNothing().when(applicationEventPublisher).publishEvent(ApplicationEvent.class);
-    auditLoggingFilter.doFilterInternal(request, response, filterChain);
+  public void testDoFilterInternal_actionpassed_auditloggingfilterdetails() {
+    try {
+      doNothing().when(filterChain).doFilter(request, response);
+      Mockito.when(auditLoggingFilterDetails.getAction())
+          .thenReturn("principal" + " " + "GET" + "path");
+      mockxFwdAndVersion();
+      Mockito.when(auditLoggingFilterDetails.getSdbNameSlug()).thenReturn("sdbNameSlug");
+      doNothing().when(applicationEventPublisher).publishEvent(ApplicationEvent.class);
+      auditLoggingFilter.doFilterInternal(request, response, filterChain);
+      assertNotNull(SecurityContextHolder.getContext());
+    } catch (Exception exception) {
+      assertNull(exception);
+    }
   }
 
   @After
