3b48fef892e86019d551715afbf0710c7c7a1fb5
Melana Hammel <melana.hammel@nike.com>
2021-04-16 09:06:16 -0700

    feat: set up call to exchange token in dashboard

--- cerberus-dashboard/src/constants/cms.js
+++ cerberus-dashboard/src/constants/cms.js
@@ -24,6 +24,7 @@ export const RETRIEVE_ROLE_PATH = '/v1/role'
 export const BUCKET_RESOURCE = '/v2/safe-deposit-box'
 export const RETRIEVE_METADATA = '/v1/metadata'
 export const MFA_REQUIRED_STATUS = 'mfa_req'
+export const TOKEN_EXCHANGE_PATH = '/v2/auth/user/oauth/exchange'
 
 export const SDB_NAME_MAX_LENGTH = 100
 export const SDB_DESC_MAX_LENGTH = 1000
